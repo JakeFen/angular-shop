98f160035bedba0640041418f4e9d6887035979b
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-06-09 20:49:47 -0400

    Added express to dependency

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const app = express();
-const http = require("http");
 const path = require("path");
+const app = express();
 
 const port = process.env.PORT || 8080;
 
@@ -9,10 +8,10 @@ app.use(express.static(__dirname + "/dist/oshop"));
 
 // PathLocationStrategy
 
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname + "/dist/oshop/index.html"))
-);
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/oshop/index.html"));
+});
 
-const server = http.createServer(app);
-
-server.listen(port, () => console.log("Running..."));
+app.listen(port, function () {
+  console.log("App listening on PORT " + port);
+});
