043626c6feaf5588e8634ed3e92452a6841a3402
Jacob Fenwick <jacobfenwick111@gmail.com>
2020-06-09 20:19:02 -0400

    Added routing to server to run on heroku

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const app = express();
 const http = require("http");
 const path = require("path");
 
-const app = express();
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/dist/oshop"));
 
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
+// PathLocationStrategy
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/dist/oshop/index.html"))
+);
 
 const server = http.createServer(app);
 
